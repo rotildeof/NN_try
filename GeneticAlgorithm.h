27522cbf7f70c8275f6f4362e137083bc2d92389
rotildeof <49618478+rotildeof@users.noreply.github.com>
2020-08-07 16:57:39 +0000

    GeneticAlgorithm.h
    Class Header file of Genetic Algorithm

---
+++ GeneticAlgorithm.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <iterator>
+#include <random>
+
+#ifndef _GeneticAlgorithm_
+#define _GeneticAlgorithm_
+
+// You should use <int> or <double>.
+
+template <typename T>
+class GeneticAlgorithm{
+public:
+  GeneticAlgorithm(int gene_length, int population);
+  ~GeneticAlgorithm(){};
+
+  
+  void GiveScore(int ith_creature, double score);
+  void GeneInitialization(T min, T max);
+
+  typename std::vector<T>::iterator GetGeneIterator(int ith_creature);
+
+  void CrossOver(int numDominantGene, double mutation_prob, std::string optimization_option);
+  private:
+
+
+  T min_;
+  T max_;
+  
+  std::mt19937 mt;
+  T type_keeper;
+  struct Creature{
+    std::vector<T> gene;
+    double Score;
+  };
+
+  std::vector<Creature> creatures;  
+
+  void Mutation(std::vector<T> &v);
+
+};
+
+#endif
