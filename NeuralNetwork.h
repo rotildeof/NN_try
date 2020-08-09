bad7194ca7d5897399975df8847a322bb44c2bb2
rotildeof <49618478+rotildeof@users.noreply.github.com>
2020-08-09 12:11:18 +0000

    Update NeuralNetwork.h
    色々と追加しました。
    活性化関数を隠れ層と出力層で分けて定義できるようにしました。
    活性化関数には、ReLU、Identity、Sigmoid、Softmax(出力層のみ)を用意しました。
    また、損失関数も平均二乗誤差以外に、2値クロスエントロピー、および多値クロスエントロピーを用意しました。

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -19,10 +19,13 @@ public:
   NeuralNetwork(std::string structure_);
   ~NeuralNetwork(){};
 
-
   void SetPopulation(int population){population_ = population;};
   void SetMutationProbability(double prob){mutation_prob = prob;};
   void SetNumOfDominantGene(double nDominantGene_){nDominantGene = nDominantGene_;};
+  
+  void SetLossFunction(std::string nameLossFunction_);
+  void SetActivationFunction_Hidden(std::string actFuncHidden);
+  void SetActivationFunction_Output(std::string actFuncOutput);
   //[i][j] : [i] --> Data entry, [j] --> ith Neuron in first (last) layer.
   void Learning(vec2D const &inputDataSet,
 		vec2D const &answerDataSet,
@@ -30,7 +33,6 @@ public:
   void Learning(vec2D const &inputDataSet,
 		vec2D const &answerDataSet,
 		double threshold);
-
   
   void SetRangeOfWeight(double min, double max){lower = min; upper = max;};
   void PrintWeightMatrix();
@@ -38,30 +40,47 @@ public:
   void PrintLastLayer(vec1D const &inputData);
   vec1D::iterator GetOutputIterator(vec1D const &inputData);
   
-private:
+  private:
   std::string structure;
   // 3:3:2
   vec3D w;
   vec2D b;
   vec2D nLayerNeurons;
-  
-  void CalcuToNextLayerNeurons(int ith_connection);
+
+  int numLastNeurons;
+  void CalcuHiddenLayer();
   void CalculationAllStageOfLayer();
   void ParameterInitialization();
   double lower = -5.; // default
   double upper =  5.; // default
   template <class T> void InputData(std::vector<T> const &indata);
-  double Sigmoid(double x);
+  static double Sigmoid(double x);
+  static double ReLU(double x);
+  static double Identity(double x);
+  static void Sigmoid(vec1D &lastLayer, vec1D const &beforeActF);
+  static void Softmax(vec1D &lastLayer, vec1D const &beforeActF);
+  static void Identity(vec1D &lastLayer, vec1D const &beforeActF);
+  double (*hidf_ptr)(double x) = &NeuralNetwork::ReLU;  // "Sigmoid", "ReLU"
+  void (*outf_ptr)(vec1D &lL, vec1D const &bAF) = &NeuralNetwork::Sigmoid;
   double population_ = 100; //default
   double mutation_prob = 0.05; // default
   double nDominantGene = 5; //default
+
   std::mt19937 mt;
 
   GeneticAlgorithm<double> GA;
-  double ErrorEvaluation(vec1D const &lastNeurons, vec1D const &answerData);
+  // ---- Loss Function ---- //
+  double (*loss_ptr)(vec1D const &lastNeurons, vec1D const &answerData)
+  = &NeuralNetwork::MeanSquaredError;
+  static double MeanSquaredError(vec1D const &lastNeurons, vec1D const &answerData); // MSE
+  static double BinaryCrossEntropy(vec1D const &lastNeurons, vec1D const &answerData); // BCE
+  static double CategoricalCrossEntropy(vec1D const &lastNeurons, vec1D const &answerData); //CCE
+  // ---------------------- //
   void SetWeightFromGene(GeneticAlgorithm<double> &GA, int ith_creature);
   void ShowInputAndOutput(GeneticAlgorithm<double> &GA, int ith_creature,
 			  vec2D const &input);
+
+  double LIMITLESS_1 = 0.9999999999999999;
 };
 
 #endif
