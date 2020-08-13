269d26d79e4b94477d60172a21109b8a28664e1b
rotildeof <49618478+rotildeof@users.noreply.github.com>
2020-08-13 10:43:36 +0000

    Update NeuralNetwork.cxx

--- NeuralNetwork.cxx
+++ NeuralNetwork.cxx
@@ -19,20 +19,42 @@ NeuralNetwork::NeuralNetwork(std::string structure_){
   }
   w.assign(neurons.size() - 1, vec2D(0, vec1D() ) );
   b.assign(neurons.size() - 1, vec1D() );
+  dw.assign(neurons.size() - 1, vec2D(0, vec1D() ) );
+  db.assign(neurons.size() - 1, vec1D() );
+  dweight.assign(neurons.size() - 1, vec2D(0, vec1D() ) );
+  doffset.assign(neurons.size() - 1, vec1D() );
+  m_w.assign(neurons.size() - 1, vec2D(0, vec1D() ) ); // Adam
+  v_w.assign(neurons.size() - 1, vec2D(0, vec1D() ) ); // Adam
+  m_b.assign(neurons.size() - 1, vec1D() ); // Adam
+  v_b.assign(neurons.size() - 1, vec1D() ); // Adam
   std::cout << "NN Structure --> " << structure << "  Number of Layers --> " << neurons.size() << std::endl;
   for(int i = 0 ; i < (int)w.size() ; i++){
-    w[i].assign(neurons[i+1], vec1D(neurons[i]) );
-    b[i].assign(neurons[i+1],0);
+    w[i].assign(neurons[i+1], vec1D(neurons[i]) ); // weight
+    b[i].assign(neurons[i+1],0);                   // offset
+    dw[i].assign(neurons[i+1], vec1D(neurons[i]) ); // dE/dw
+    db[i].assign(neurons[i+1],0);                   // dE/db
+    dweight[i].assign(neurons[i+1], vec1D(neurons[i]) ); // Momentum SGD
+    doffset[i].assign(neurons[i+1], 0 );                 // Momentum SGD
+    m_w[i].assign(neurons[i+1], vec1D(neurons[i]) ); // Adam
+    v_w[i].assign(neurons[i+1], vec1D(neurons[i]) ); // Adam
+    m_b[i].assign(neurons[i+1],0);                   // Adam
+    v_b[i].assign(neurons[i+1],0);                   // Adam
     std::cout << i << " : " << "Weight Matrix --> [" << neurons[i+1] << "][" << neurons[i] << "]  Offset Vector --> [" << neurons[i+1] << "]" << std::endl;
   }
   
   nLayerNeurons.assign(neurons.size(), vec1D());
+  beforeActFunc.assign(neurons.size(), vec1D());
+  delta        .assign(neurons.size(), vec1D());
   for(int i = 0 ; i < (int)nLayerNeurons.size() ; i++){
     nLayerNeurons[i].assign(neurons[i], 0);
+    beforeActFunc[i].assign(neurons[i], 0); // nLayerZ[0][i] = 0;
+    delta        [i].assign(neurons[i], 0); // delta for back propagation
   }
 
   numLastNeurons = *(neurons.end() -1);
-  
+
+  deltaCalcu_ptr = &NeuralNetwork::CalculationLastLayerDelta;
+  method_ptr     = &NeuralNetwork::MomentumSGD;
   std::random_device rd;
   mt = std::mt19937( rd() );
 
@@ -41,7 +63,6 @@ NeuralNetwork::NeuralNetwork(std::string structure_){
     gene_length += nLayerNeurons[i].size() * nLayerNeurons[i+1].size()
       + nLayerNeurons[i+1].size() ;
   }
-  std::cout << "gene length = " << gene_length << std::endl;
   
 }
 
@@ -50,6 +71,8 @@ double NeuralNetwork::Sigmoid(double x){
   double y = 1. / ( 1 + std::exp( - x ) );
   if(y == 1){
     y = 0.9999999999999999;
+  }else if(y == 0){
+    y = 1e-323;
   }
 
   return y;
@@ -67,6 +90,22 @@ double NeuralNetwork::Identity(double x){
   return x;
 }
 
+double NeuralNetwork::DSigmoid(double x){
+  return Sigmoid(x) * (1 - Sigmoid(x) );
+}
+
+double NeuralNetwork::DReLU(double x){
+  if(x >=0){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+double NeuralNetwork::DIdentity(double x){
+  return 1;
+}
+
 void NeuralNetwork::Sigmoid(vec1D &lastLayer, vec1D const &beforeActF){
   for(int i = 0 ; i < (int)beforeActF.size() ; i++){
     lastLayer[i] = 1. / ( 1 + std::exp( - beforeActF[i] ) ) ;
@@ -79,6 +118,7 @@ void NeuralNetwork::Sigmoid(vec1D &lastLayer, vec1D const &beforeActF){
   }
   return;
 }
+
 
 void NeuralNetwork::Softmax(vec1D &lastLayer, vec1D const &beforeActF){
   auto it_max = std::max_element(beforeActF.begin(), beforeActF.end());
@@ -95,6 +135,7 @@ void NeuralNetwork::Softmax(vec1D &lastLayer, vec1D const &beforeActF){
   return;
 }
 
+
 void NeuralNetwork::Identity(vec1D &lastLayer, vec1D const &beforeActF){
 
   for(int i = 0 ; i < (int)beforeActF.size() ; i++){
@@ -105,11 +146,14 @@ void NeuralNetwork::Identity(vec1D &lastLayer, vec1D const &beforeActF){
 
 void NeuralNetwork::SetActivationFunction_Hidden(std::string actFuncHidden){
   if(actFuncHidden == "Sigmoid"){
-    hidf_ptr = &NeuralNetwork::Sigmoid;
+    hidf_ptr  = &NeuralNetwork::Sigmoid;
+    hiddf_ptr = &NeuralNetwork::DSigmoid;
   }else if(actFuncHidden == "ReLU"){
-    hidf_ptr = &NeuralNetwork::ReLU;
+    hidf_ptr  = &NeuralNetwork::ReLU;
+    hiddf_ptr = &NeuralNetwork::DReLU;
   }else if(actFuncHidden == "Identity"){
-    hidf_ptr = &NeuralNetwork::Identity;
+    hidf_ptr  = &NeuralNetwork::Identity;
+    hiddf_ptr = &NeuralNetwork::DIdentity;
   }else{
     std::cout << "No valid function name was input !" << std::endl;
   }
@@ -118,11 +162,17 @@ void NeuralNetwork::SetActivationFunction_Hidden(std::string actFuncHidden){
 
 void NeuralNetwork::SetActivationFunction_Output(std::string actFuncOutput){
   if(actFuncOutput == "Sigmoid"){
-    outf_ptr = &NeuralNetwork::Sigmoid;
+    outf_ptr  = &NeuralNetwork::Sigmoid;
+    outdf_ptr = &NeuralNetwork::DSigmoid;
+    deltaCalcu_ptr = &NeuralNetwork::CalculationLastLayerDelta;
   }else if(actFuncOutput == "Softmax"){
-    outf_ptr = &NeuralNetwork::Softmax;
+    outf_ptr  = &NeuralNetwork::Softmax;
+    deltaCalcu_ptr = &NeuralNetwork::CalculationLastLayerDeltaSoftmax;
+    outdf_ptr = NULL;
   }else if(actFuncOutput == "Identity"){
-    outf_ptr = &NeuralNetwork::Identity;
+    outf_ptr  = &NeuralNetwork::Identity;
+    outdf_ptr = &NeuralNetwork::DIdentity;
+    deltaCalcu_ptr = &NeuralNetwork::CalculationLastLayerDelta;
   }else{
     std::cout << "No valid function name was input !" << std::endl;
   }
@@ -131,15 +181,31 @@ void NeuralNetwork::SetActivationFunction_Output(std::string actFuncOutput){
 
 void NeuralNetwork::SetLossFunction(std::string nameLossFunction_){
   if(nameLossFunction_ == "MSE"){
-    loss_ptr = &NeuralNetwork::MeanSquaredError;
+    loss_ptr  = &NeuralNetwork::MeanSquaredError;
+    dloss_ptr = &NeuralNetwork::DMSE;
   }else if(nameLossFunction_ == "BCE"){
-    loss_ptr = &NeuralNetwork::BinaryCrossEntropy;
+    loss_ptr  = &NeuralNetwork::BinaryCrossEntropy;
+    dloss_ptr = &NeuralNetwork::DBCE;
   }else if(nameLossFunction_ == "CCE"){
-    loss_ptr = &NeuralNetwork::CategoricalCrossEntropy;
+    loss_ptr  = &NeuralNetwork::CategoricalCrossEntropy;
+    dloss_ptr = &NeuralNetwork::DCCE;
   }else{
     std::cout << "No valid function name was input !" << std::endl;
   }
 
+}
+
+void NeuralNetwork::SetLearningMethod(std::string method){
+  if(method == "MomentumSGD"){
+    method_ptr = &NeuralNetwork::MomentumSGD;
+  }else if(method == "Adam"){
+    method_ptr = &NeuralNetwork::Adam;
+  }else if(method == "SGD"){
+    method_ptr = &NeuralNetwork::SGD;
+  }else{
+    std::cout << "No valid method name was input !" << std::endl;
+  }
+   
 }
 
 void NeuralNetwork::CalcuHiddenLayer(){
@@ -151,6 +217,7 @@ void NeuralNetwork::CalcuHiddenLayer(){
 	sum += nLayerNeurons[ith_connection][j] * w[ith_connection][i][j];
       }
       sum += b[ith_connection][i];
+      beforeActFunc[ith_connection + 1][i] = sum;
       nLayerNeurons[ith_connection + 1][i] = hidf_ptr(sum);
     }
   }
@@ -162,17 +229,16 @@ void NeuralNetwork::CalculationAllStageOfLayer(){
   CalcuHiddenLayer();
   // --- Last Layer --//
   int last_con = (int)w.size() - 1;
-  vec1D beforeActFunc;
+
   for(int i = 0 ; i < (int)w[last_con].size() ; i++){ 
     double sum = 0;
     for(int j = 0 ; j < (int)w[last_con][i].size() ; j++){
       sum += nLayerNeurons[last_con][j] * w[last_con][i][j];
     }
     sum += b[last_con][i];
-    beforeActFunc.push_back(sum);
-    //nLayerNeurons[ith_connection + 1][i] = hidf_ptr(sum);
+    beforeActFunc[last_con + 1][i] = sum;
   }
-  outf_ptr(nLayerNeurons[last_con + 1], beforeActFunc);
+  outf_ptr(nLayerNeurons[last_con + 1], beforeActFunc[last_con + 1]);
   
 }
 
@@ -206,6 +272,148 @@ void NeuralNetwork::ParameterInitialization(){
   return;
 }
 
+void NeuralNetwork::TrainNN(vec2D const &inputDataSet, vec2D const &answerDataSet, int nRepetitions){
+  long long nEntries = (long long)inputDataSet.size();
+  if(nEntries != (int)answerDataSet.size()){
+    std::cout << "-- Error !! Number of Entries are different between input data and answer data !! --" << std::endl;
+    return;
+  }
+  int last = (int)nLayerNeurons.size() - 1;
+  std::uniform_int_distribution<> uni(0, nEntries - 1);
+  ParameterInitialization();
+  double acc = 0;
+  for(int iLearn = 0 ; iLearn < nRepetitions ; iLearn++){
+    int iEntry = uni(mt);
+    InputData(inputDataSet[iEntry]);	
+    CalculationAllStageOfLayer();
+    double error = loss_ptr( nLayerNeurons[last], answerDataSet[iEntry]) / numLastNeurons;
+    acc += error;
+    if(iLearn % 10000 == 0){
+      std::cout << "Error (Average) = " << acc / 10000 << std::endl;
+      acc = 0;
+    }
+    
+    //CalculationLastLayerDelta(nLayerNeurons[last], answerDataSet[iEntry]);
+
+    (this ->*deltaCalcu_ptr)(nLayerNeurons[last], answerDataSet[iEntry]);
+    CalculationAllLayerDelta();
+    //-- here back probagation -- //
+    CalcuGradientW();
+    //BackPropagation();
+    (this ->*method_ptr)();
+
+  }
+
+
+}
+
+void NeuralNetwork::CalculationLastLayerDelta(vec1D const &NNoutput, vec1D const &answer){
+  std::size_t last = delta.size() - 1 ;
+  //std::vector<double> vDOutFunc(delta.size());
+  //outdf_ptr(vDOutFunc, beforeActFunc[last]);
+  for(int i = 0 ; i < (int)delta[last].size() ; i++){
+    delta[last][i] = dloss_ptr(NNoutput[i], answer[i]) * outdf_ptr(beforeActFunc[last][i]);
+  }
+  return;
+}
+
+void NeuralNetwork::CalculationLastLayerDeltaSoftmax(vec1D const &NNoutput, vec1D const &answer){
+  int last = delta.size() - 1;
+  int K = nLayerNeurons[last].size();
+  for(int i = 0 ; i < K ; i++){ // to determine delta[i]
+    double acc = 0;
+    for(int k = 0 ; k < K ; k++){
+      if(i == k){
+	acc += dloss_ptr(NNoutput[k], answer[k]) * NNoutput[i] * ( 1 - NNoutput[i] );
+      }else{
+	acc += dloss_ptr(NNoutput[k], answer[k]) * ( - NNoutput[i] * NNoutput[k]);
+      }
+    }
+    delta[last][i] = acc;
+  }
+  return;
+}
+
+void NeuralNetwork::CalculationAllLayerDelta(){
+  int last = delta.size() - 1;
+  // delta[last] was already calculated in function "CalculationLastLayerDelta".
+  for(int L = last - 1 ; L >= 1 ; L--){
+    for(int i = 0 ; i < (int)delta[L].size() ; i++){
+      double acc = 0;
+      for(int k = 0 ; k < (int)delta[L + 1].size() ; k++){
+	acc += delta[L + 1][k] * w[L][k][i];
+      }
+      delta[L][i] = acc * hiddf_ptr(beforeActFunc[L][i]); 
+    }
+  }
+  return;
+}
+
+void NeuralNetwork::CalcuGradientW(){
+  for(int L = 0 ; L < (int)w.size() ; L++){
+    for(int i = 0 ; i < (int)w[L].size(); i++){
+      for(int j = 0 ; j < (int)w[L][i].size() ; j++){
+	dw[L][i][j] = delta[L + 1][i] * nLayerNeurons[L][j];
+      }
+      db[L][i] = delta[L + 1][i];
+    }
+  }
+  return;
+}
+
+void NeuralNetwork::SGD(){
+  for(int L = 0 ; L < (int)w.size() ; L++){
+    for(int i = 0 ; i < (int)w[L].size(); i++){
+      for(int j = 0 ; j < (int)w[L][i].size() ; j++){
+	w[L][i][j] = w[L][i][j] - learningRate * dw[L][i][j];
+      }
+      b[L][i] = b[L][i] - learningRate * db[L][i];
+    }
+  }
+  return;
+}
+
+void NeuralNetwork::MomentumSGD(){
+  for(int L = 0 ; L < (int)w.size() ; L++){
+    for(int i = 0 ; i < (int)w[L].size(); i++){
+      for(int j = 0 ; j < (int)w[L][i].size() ; j++){
+	double tempW = dw[L][i][j];
+	dweight[L][i][j] = tempW * learningRate + alpha * dweight[L][i][j];
+	w[L][i][j] = w[L][i][j] - dweight[L][i][j];
+      }
+      double tempB = db[L][i];
+      doffset[L][i] = tempB * learningRate + alpha * doffset[L][i];
+      b[L][i] = b[L][i] - doffset[L][i];
+    }
+  }
+  return;
+}
+
+void NeuralNetwork::Adam(){
+  static int t = 1;
+  double hat_deno_beta_1 = (1 - std::pow(beta_1, t) );
+  double hat_deno_beta_2 = (1 - std::pow(beta_2, t) );
+  for(int L = 0 ; L < (int)w.size() ; L++){
+    for(int i = 0 ; i < (int)w[L].size(); i++){
+      for(int j = 0 ; j < (int)w[L][i].size() ; j++){
+	m_w[L][i][j] = beta_1 * m_w[L][i][j] + (1 - beta_1) * dw[L][i][j];
+	v_w[L][i][j] = beta_2 * v_w[L][i][j] + (1 - beta_2) * dw[L][i][j] * dw[L][i][j];
+	double m_hat = m_w[L][i][j] / hat_deno_beta_1;
+	double v_hat = v_w[L][i][j] / hat_deno_beta_2;
+	w[L][i][j] = w[L][i][j] - alpha_Adam * m_hat / (std::sqrt(v_hat) + epsilon);
+      }
+      m_b[L][i] = beta_1 * m_b[L][i] + (1 - beta_1) * db[L][i];
+      v_b[L][i] = beta_2 * v_b[L][i] + (1 - beta_2) * db[L][i] * db[L][i];
+      double m_hat = m_b[L][i] / hat_deno_beta_1;
+      double v_hat = v_b[L][i] / hat_deno_beta_2;
+      b[L][i] = b[L][i] - alpha_Adam * m_hat / (std::sqrt(v_hat) + epsilon);
+    }
+  }
+  t++;
+  return;
+
+}
+
 void NeuralNetwork::Learning(vec2D const &inputDataSet, vec2D const &answerDataSet, int nRepetitions){
   long long nEntries = (long long)inputDataSet.size();
   if(nEntries != (int)answerDataSet.size()){
@@ -221,7 +429,7 @@ void NeuralNetwork::Learning(vec2D const &inputDataSet, vec2D const &answerDataS
     gene_length += nLayerNeurons[i].size() * nLayerNeurons[i+1].size()
       + nLayerNeurons[i+1].size() ;
   }
-  
+  std::cout << "gene length = " << gene_length << std::endl;
   GA = GeneticAlgorithm<double>(gene_length, population_);
   GA.GeneInitialization(lower, upper);
 
@@ -364,6 +572,17 @@ double NeuralNetwork::CategoricalCrossEntropy(vec1D const &lastNeurons, vec1D co
   return acc;
 }
 
+double NeuralNetwork::DMSE(double y, double d){
+  return 2 * (y - d);
+}
+
+double NeuralNetwork::DBCE(double y, double d){
+  return -1 * ( d - y ) / ( y * ( 1 - y ) );
+}
+
+double NeuralNetwork::DCCE(double y, double d){
+  return -1 * ( d - y ) / ( y * ( 1 - y ) );
+}
 
 void NeuralNetwork::ShowInputAndOutput(GeneticAlgorithm<double> &GA, int ith_creature, vec2D const &input){
   SetWeightFromGene(GA, ith_creature);
